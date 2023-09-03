d3c8fd2b808de1c58443467aca9e0fe4777d7590
Saskar Khadka <saskar.khadka@gmail.com>
2023-09-04 01:51:29 +0545

    [feat] multithreading for uploading image and sending sms

--- fire.py
+++ fire.py
@@ -7,65 +7,91 @@ from pathlib import Path
 from imgur.working_imgur import upload_image
 import datetime
 import requests
+import threading
 
 
-BASE_DIR = Path(__file__).resolve().parent
+def upload_image_send_sms(image_path):
+    result = upload_image(image_path)
+    phone_no = "+9779841939234"
+    try:
+        requests.get(
+            f"http://192.168.18.218/firedetected?number={phone_no}&data={result}"
+        )
+    except Exception as e:
+        pass
 
-image_directory = os.path.join(BASE_DIR, "saved_infer_image")
 
+def main():
+    BASE_DIR = Path(__file__).resolve().parent
 
-# Running real time from webcam
-cap = cv2.VideoCapture(0)
-model = YOLO("fire.pt")
+    image_directory = os.path.join(BASE_DIR, "saved_infer_image")
 
-past = datetime.datetime.now()
+    # Running real time from webcam
+    cap = cv2.VideoCapture(0)
+    model = YOLO("fire.pt")
 
-# Reading the classes
-classnames = ["fire"]
+    past = datetime.datetime.now()
 
-capture_image = True
+    # Reading the classes
+    classnames = ["fire"]
 
-while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, (640, 480))
-    result = model(frame, stream=True)
+    capture_image = True
 
-    # Getting bbox,confidence and class names informations to work with
-    for info in result:
-        boxes = info.boxes
-        for box in boxes:
-            confidence = box.conf[0]
-            confidence = math.ceil(confidence * 100)
-            Class = int(box.cls[0])
-            if confidence > 50:
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 5)
-                cvzone.putTextRect(
-                    frame,
-                    f"{classnames[Class]} {confidence}%",
-                    [x1 + 8, y1 + 100],
-                    scale=1.5,
-                    thickness=2,
-                )
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (640, 480))
+        result = model(frame, stream=True)
 
-                if confidence > 65 and capture_image:
-                    success, image = cap.read()
-                    if success:
-                        past = datetime.datetime.now()
-                        cv2.imwrite(f"{image_directory}/cap_img.png", image)
-                        result = upload_image(f"{image_directory}/cap_img.png")
-                        phone_no = "+9779861285984"
-                        try:
-                            res = requests.get(
-                                f"http://192.168.18.218/firedetected?number={phone_no}&data={result}"
+        # Getting bbox,confidence and class names informations to work with
+        for info in result:
+            boxes = info.boxes
+            for box in boxes:
+                confidence = box.conf[0]
+                confidence = math.ceil(confidence * 100)
+                Class = int(box.cls[0])
+                if confidence > 50:
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 5)
+                    cvzone.putTextRect(
+                        frame,
+                        f"{classnames[Class]} {confidence}%",
+                        [x1 + 8, y1 + 100],
+                        scale=1.5,
+                        thickness=2,
+                    )
+
+                    if confidence > 65 and capture_image:
+                        success, image = cap.read()
+                        if success:
+                            past = datetime.datetime.now()
+                            cv2.imwrite(f"{image_directory}/cap_img.png", image)
+                            # asyncio.run(upload_image(f"{image_directory}/cap_img.png"))
+                            # thread = threading.Thread(
+                            #     target=upload_image,
+                            #     args=(f"{image_directory}/cap_img.png"),
+                            # )
+                            thread = threading.Thread(
+                                target=upload_image_send_sms,
+                                args=(f"{image_directory}/cap_img.png",),
                             )
-                        except Exception as e:
-                            pass
-                        capture_image = False
+                            thread.start()
+                            # result = upload_image(f"{image_directory}/cap_img.png")
+                            # phone_no = "+9779840172217"
+                            # try:
+                            #     res = requests.get(
+                            #         f"http://192.168.18.218/firedetected?number={phone_no}&data={result}"
+                            #     )
+                            # except Exception as e:
+                            #     pass
+                            capture_image = False
 
-    time_diff = datetime.datetime.now() - past
-    if time_diff.total_seconds() / 60 > 1:
-        capture_image = True
-    cv2.imshow("frame", frame)
-    cv2.waitKey(1)
+        time_diff = datetime.datetime.now() - past
+        if time_diff.total_seconds() / 60 > 1:
+            capture_image = True
+        cv2.imshow("frame", frame)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    (main())
